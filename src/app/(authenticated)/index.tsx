a0454edccea25c6591bb614ce42cc62442c1ca7c
Daniel Israel <danny.israel@gmail.com>
2025-04-27 09:20:47 -0700

    feat: 🎸 container structure for messages page

--- src/app/(authenticated)/index.tsx
+++ src/app/(authenticated)/index.tsx
@@ -14,28 +14,28 @@ export default function Home() {
     };
 
     return (
-        <View className="flex-1 bg-primary">
+        <View className="flex-1 bg-slate-400">
+            {/* Outer container that centers content */}
+            <View className="flex-1 items-center">
+                {/* Inner container with max width */}
+                <View className="flex-1 w-full max-w-3xl">
+                    {/* White header with notch area */}
+                    <SafeAreaView className="bg-white pb-2 shadow-md rounded- z-10 lg:rounded-b-md" edges={['top']}>
+                        <View className="w-full items-center justify-center lg:py-4">
+                            <Text className="text-lg text-dark font-semibold">Messages</Text>
+                        </View>
+                    </SafeAreaView>
 
-            <View className="flex-1 border">
-
-                {/* White header with notch area */}
-                <SafeAreaView className="bg-primary pb-2 shadow-md rounded- z-10 " edges={['top']}>
-                    <View className="w-full items-center justify-center">
-                        <Text className="text-lg text-light font-semibold">Messages</Text>
-                    </View>
-                </SafeAreaView>
-
-                {/* Main content area */}
-                <View className="flex-1 bg-gray-100">
-                    <View className="p-4">
-                        <Pressable className="bg-primary px-4 py-2 rounded-md" onPress={handleSignOut}>
-                            <Text className="text-white">Sign Out</Text>
-                        </Pressable>
+                    {/* Main content area */}
+                    <View className="flex-1 bg-slate-200 border-black/50 border lg:-mt-1">
+                        <View className="p-4">
+                            <Pressable className="bg-primary px-4 py-2 rounded-md flex-row items-center gap-2 self-start" onPress={handleSignOut}>
+                                <Text className="text-white">Sign Out</Text>
+                            </Pressable>
+                        </View>
                     </View>
                 </View>
-
             </View>
-
         </View>
     );
 }
